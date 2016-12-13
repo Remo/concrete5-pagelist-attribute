505a4ab5e40ad48dcb3f8f3356b3cfd88f2ff340
thomas <thomas.pluess@mesch.ch>
2016-12-13 17:34:25 +0100

    make sure a language can be selected to filter the related pages

--- models/attribute/types/remo_page_list_attribute/type_form.php
+++ models/attribute/types/remo_page_list_attribute/type_form.php
@@ -4,36 +4,44 @@ defined('C5_EXECUTE') or die('Access Denied.');
 $selectedPageTypes = preg_split('[,]', $akSelectedPageTypes);
 ?>
 <fieldset>
-	<legend><?php echo t('Select Options') ?></legend>
+    <legend><?php echo t('Select Options') ?></legend>
 
-	<div class="clearfix">
-		<label><?php echo t("Page Type") ?></label>
-		<div class="input">
-			<ul class="inputs-list">
-                                <?php foreach ($collectionTypes as $key => $collectionType) { ?>
-                                        <li>
-                                                <label for="selectedPageTypes_<?php echo $key ?>">
-                                                        <input <?php echo in_array($collectionType->getCollectionTypeID(), $selectedPageTypes) ? ' checked="checked" ' : '' ?> id="selectedPageTypes_<?php echo $key ?>" type="checkbox" name="selectedPageTypes[]" value="<?php echo $collectionType->getCollectionTypeID() ?>"/>
-                                                        <?php echo $collectionType->getCollectionTypeName() ?>
-                                                </label>
-                                        </li>
-                                <?php } ?>
-			</ul>
-		</div>
-	</div>
+    <div class="clearfix">
+        <label><?php echo t("Page Type") ?></label>
+        <div class="input">
+            <ul class="inputs-list">
+                <?php foreach ($collectionTypes as $key => $collectionType) { ?>
+                    <li>
+                        <label for="selectedPageTypes_<?php echo $key ?>">
+                            <input <?php echo in_array($collectionType->getCollectionTypeID(), $selectedPageTypes) ? ' checked="checked" ' : '' ?> id="selectedPageTypes_<?php echo $key ?>" type="checkbox" name="selectedPageTypes[]" value="<?php echo $collectionType->getCollectionTypeID() ?>"/>
+                            <?php echo $collectionType->getCollectionTypeName() ?>
+                        </label>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+    <?php if (!empty($multilingualSections)): ?>
+        <div class="clearfix">
+            <label><?php echo t('Language'); ?></label>
+            <div class="input">
+                <?= $form->select('displayMultilingualSection', $multilingualSections, 0); ?>
+            </div>
+        </div>
+    <?php endif; ?>
 
-	<div class="clearfix">
-		<label><?php echo t("Display Drop-Down") ?></label>
-		<div class="input">
-			<ul class="inputs-list">
-                            <li>
-                                <label>
-                                    <?php echo $form->checkbox('displayDropDown', 1, $akDisplayDropDown)?> 
-                                    <span><?=t('Always display drop down in search form.')?>
-                                    </span>
-                                </label>
-                            </li>
-                        </ul>
-		</div>
-	</div>        
+    <div class="clearfix">
+        <label><?php echo t("Display Drop-Down") ?></label>
+        <div class="input">
+            <ul class="inputs-list">
+                <li>
+                    <label>
+                        <?php echo $form->checkbox('displayDropDown', 1, $akDisplayDropDown) ?> 
+                        <span><?= t('Always display drop down in search form.') ?>
+                        </span>
+                    </label>
+                </li>
+            </ul>
+        </div>
+    </div>        
 </fieldset>
